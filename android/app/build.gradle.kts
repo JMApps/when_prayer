a8f9a74117cd9c8b771d4de97c22d451581fe7d9
JMApps <jmappsmuslim@gmail.com>
2026-02-15 13:21:44 +0300

    New Android version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.taqweem.when_prayer"
         minSdk = 25
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     buildTypes {
